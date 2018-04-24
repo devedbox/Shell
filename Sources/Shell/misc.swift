b56a27f989839e74312058243fd31df99a525890
devedbox <devedbox@qq.com>
2018-04-24 22:28:59 +0800

    Added misc.swift file.

---
+++ Sources/Shell/misc.swift
@@ -0,0 +1,44 @@
+//
+//  misc.swift
+//  Shell
+//
+//  Created by devedbox on 2018/4/24.
+//
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+import Foundation
+
+/// Returns the environment variable path of the system if any.
+internal var _envPaths: [String] = { () -> [String] in
+    let env_paths = getenv("PATH")
+    let char_env_paths = unsafeBitCast(env_paths, to: UnsafePointer<CChar>.self)
+    #if swift(>=4.1)
+    return
+        String(validatingUTF8: char_env_paths)?
+            .split(separator: ":")
+            .compactMap({ String($0) })
+            ?? []
+    #else
+    return
+    String(validatingUTF8: char_env_paths)?
+    .split(separator: ":")
+    .flatMap({ String($0) })
+    ?? []
+    #endif
+}()
+/// Find the executable path with a path extension.
+internal func _executable(_ name: String) -> String? {
+    let paths =
+        [FileManager.default.currentDirectoryPath] + _envPaths
+    return
+        paths.map({
+            name.hasPrefix("/")
+                ? $0 + name
+                : $0 + "/\(name)"
+        }).first(where: {
+            FileManager.default.isExecutableFile(atPath: $0)
+        })
+}
