33017f41827b7feb45c786e7444913041bc91891
devedbox <devedbox@qq.com>
2018-05-04 21:53:00 +0800

    Added comments annotations.

--- Sources/Shellman/Shell.swift
+++ Sources/Shellman/Shell.swift
@@ -31,10 +31,7 @@ public struct Shell<Result: ShellResultProtocol> {
     public init(_ commands: String) {
         self.init(commands.split(separator: " ").map { String($0) })
     }
-    /// Creates new instance of `Shell` with the given command along with arguments as a `Array`.
-    ///
-    /// - Parameter commandsArgs: The array of command and arguments.
-    /// - Returns: Instance of `Shell` with the given command and args.
+    /// Creates new instance of `Shell`
     internal init(_ commandsArgs: [String]) {
         var commands = commandsArgs
         _process.launchPath = _executable(commands.removeFirst())
@@ -45,16 +42,8 @@ public struct Shell<Result: ShellResultProtocol> {
 // MARK: - ExpressibleByStringLiteral.
 
 extension Shell: ExpressibleByStringLiteral {
-    /// StringLiteralType alias of `String`.
     public typealias StringLiteralType = String
-    /// Creates an instance of `Shell` with the commands string literal. Using white space to separate
-    /// diffrent part of the commands like this:
-    /// ```
-    /// {command} {arg0} {arg1} ...{argn}
-    /// ```
-    ///
-    /// - Parameter commands: Command string along with arguments of the command.
-    /// - Returns: Instance of `Shell` with the given command and args.
+    
     public init(stringLiteral commands: String) {
         self.init(commands)
     }
@@ -62,37 +51,8 @@ extension Shell: ExpressibleByStringLiteral {
 
 // MARK: -
 
-/// Easily creates `Shell<R>` from a sting.
-///
-/// - Parameter commands: A shell commands string.
-/// - Parameter as: Generic context.
-/// - Returns: An instance of `Shell<R>` from the given shell commands string.
-public func shell<R>(_ commands: String, as: Shell<R>.Type) -> Shell<R> {
-    return Shell<R>(commands)
-}
-/// Easily creates `[Shell<R>]` from a multiline sting.
-///
-/// - Parameter multilines: A multiline shell commands string.
-/// - Parameter as: Generic context.
-/// - Returns: An instance of `[Shell<R>]` from the given shell commands string.
-public func shells<R>(_ multilines: String, as: Shell<R>.Type) -> [Shell<R>] {
-    return Shell<R>.shells(from: multilines)
-}
-
 extension Shell {
-    /// Creates an instance of `[Shell<Result>]` from a multiline string. Shell in the string is separated by `\n`.
-    /// ```
-    /// """
-    /// git clone https://github.com/apple/swift
-    /// cd ./swift
-    /// swift build -c release
-    /// cd ../
-    /// rm -rf ./swift
-    /// """
-    /// ```
-    /// - Parameter multilines: The multiline string contains shell commands.
-    /// - Returns: An instance of `[Shell<Result>]` represents the shells separated by `\n`.
-    public static func shells(from multilines: String) -> [Shell<Result>] {
+    public static func shells(_ multilines: String) -> [Shell<Result>] {
         return (multilines as NSString).components(separatedBy: .newlines).map { Shell<Result>($0) }
     }
 }
@@ -100,21 +60,17 @@ extension Shell {
 // MARK: - Public.
 
 extension Shell {
-    /// Returns the executable command of the shell.
     public var command: String? {
         guard let cmd = _process.launchPath?.split(separator: "/").last else {
             return nil
         }
         return String(cmd)
     }
-    /// Returns the arguments of command in the shell.
+    
     public var arguments: [String] {
         return _process.arguments ?? []
     }
-    /// Launch the command at a given working path as current directory path.
-    ///
-    /// - Parameter path: Current directory path as working path of the command.
-    /// - Returns: An instance of `Result` represents the result of the shell's executing.
+    
     @discardableResult
     public func execute(at path: String? = nil) -> Result {
         var result = Result()
