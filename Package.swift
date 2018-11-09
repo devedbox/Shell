03b4e642a29c86c46feeef7e1cef08a7c15ef753
devedbox <862099730@qq.com>
2018-11-10 00:12:13 +0800

    feat: Adds dependency 'Commander'.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,32 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Shellman",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Shellman",
-            targets: ["Shellman"]),
+  name: "Shellman",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "Shellman",
+      targets: ["Shellman"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/devedbox/Commander.git", "0.1.0"..<"100.0.0"),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Shellman",
-            dependencies: []),
-        .testTarget(
-            name: "ShellmanTests",
-            dependencies: ["Shellman"]),
-    ]
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "Shellman",
+      dependencies: [
+        "Commander"
+      ]
+    ),
+    .testTarget(
+      name: "ShellmanTests",
+      dependencies: ["Shellman"]
+    ),
+  ]
 )
